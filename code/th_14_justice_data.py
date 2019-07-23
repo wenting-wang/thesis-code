1dc15f6077a822b0f3d0c597ba1dff64a2c05008
wwendi <wenting.wang.cd@outlook.com>
2019-07-23 18:42:52 +0200

    update

--- code/th_14_justice_data.py
+++ code/th_14_justice_data.py
@@ -1,18 +1,20 @@
 import os
 import re
 import json
+import pandas as pd
 
 
 project_path = '/home/wenting/PycharmProjects/thesis/'
-output_path = project_path + 'data/mixed_data_justice/'
 
 justices = {'UNKN' :0, 'ALIT': 112, 'BREY': 110, 'CONN': 104, 'GINS': 109, 'KENN': 106, 'ROBE': 111,
             'SCAL': 105, 'SOUT': 107, 'STEV': 103, 'THOM': 108,'KAGA': 114, 'SOTO': 113, 'REHN': 102,
             'BLAC': 100, 'WHIT': 95, 'MARS': 98, 'BREN': 92, 'POWE': 101, 'BURG': 99, 'STEW': 94}
 
+""" code 1, get text file justice level >> text_data_justice"""
+
 
 def dict_raise_on_duplicates(ordered_pairs):
-    """Reject duplicate keys."""
+    """iterate read, not overwrite"""
     d = {}
     for k, v in ordered_pairs:
         if k in d:
@@ -23,6 +25,8 @@ def dict_raise_on_duplicates(ordered_pairs):
 
 
 def get_text_pe(filename, docket):
+    output_path = project_path + 'data/mixed_data_justice/text_data_justice/'
+
     with open(filename, 'r', encoding='utf-8') as f:
         texts = json.load(f, object_pairs_hook=dict_raise_on_duplicates)
 
@@ -37,6 +41,8 @@ def get_text_pe(filename, docket):
 
 
 def get_text_re(filename, docket):
+    output_path = project_path + 'data/mixed_data_justice/text_data_justice/'
+
     with open(filename, 'r', encoding='utf-8') as f:
         texts = json.load(f, object_pairs_hook=dict_raise_on_duplicates)
 
@@ -50,14 +56,116 @@ def get_text_re(filename, docket):
     return
 
 
-for root, dirs, files in os.walk(project_path + 'data/cleaned_data'):
-    for file in files:
-        if file.endswith("-result-1.txt"):
-            docket_num = file[:-13]
-            input_file = os.path.join(root, file)
-            get_text_pe(input_file, docket_num)
+# get text file, justice level (18305 entries, petitioner, respondent included)
+def get_text():
+    for root, dirs, files in os.walk(project_path + 'data/cleaned_data'):
+        for file in files:
+            if file.endswith("-result-1.txt"):
+                docket_num = file[:-13]
+                input_file = os.path.join(root, file)
+                get_text_pe(input_file, docket_num)
 
-        if file.endswith("-result-2.txt"):
-            docket_num = file[:-13]
-            input_file = os.path.join(root, file)
-            get_text_re(input_file, docket_num)
+            if file.endswith("-result-2.txt"):
+                docket_num = file[:-13]
+                input_file = os.path.join(root, file)
+                get_text_re(input_file, docket_num)
+# get_text()
+
+
+""" code 2, get filter from audio file, drop duplicated item in info file >> case_info_justice_filtered.csv"""
+""" code 2, get filter from audio file, drop duplicated item in info file >> audio_filtered.csv"""
+
+# filter info file, based on audio file (justice results, 5209 entries)
+# info file has duplicated items. drop them. now use 'case_info_justice_filtered' as filter! (5158!!!)
+
+
+def get_info():
+    caseinfo_jusctice = pd.read_csv(project_path + 'data/mixed_data_justice/caseinfo_justice.csv')
+    justice_results = pd.read_csv(project_path + 'data/mixed_data_justice/justice_results.csv', sep='\t')
+    docket_justice_filter = justice_results[['docket', 'justice', 'petitioner_vote']]  # filter
+
+    # get 'case_info_justice_filtered.csv'
+    caseinfo_jusctice_new = pd.merge(docket_justice_filter, caseinfo_jusctice, on=['docket', 'justice'], how='left')
+    caseinfo_jusctice_new = caseinfo_jusctice_new.drop_duplicates(['docket', 'justice'], keep=False)
+    caseinfo_jusctice_new.to_csv(project_path + 'data/mixed_data_justice/case_info_justice_filtered.csv', index=False)
+
+
+def get_audio_filtered():
+    # get 'audio_filtered.csv'
+    caseinfo_jusctice = pd.read_csv(project_path + 'data/mixed_data_justice/caseinfo_justice.csv')
+    justice_results = pd.read_csv(project_path + 'data/mixed_data_justice/justice_results.csv', sep='\t')
+    caseinfo_jusctice = caseinfo_jusctice[['docket', 'justice']]  # filter
+
+    audio_filtered = pd.merge(justice_results, caseinfo_jusctice, on=['docket', 'justice'], how='left')
+    audio_filtered = audio_filtered.drop_duplicates(['docket', 'justice'], keep=False)
+    audio_filtered.to_csv(project_path + 'data/mixed_data_justice/audio_filtered.csv', index=False)
+
+# get_info()
+# get_audio_filtered()
+
+
+""" code 3, get text file justice level, filtered by audio and info >> text_data_justice_filtered """
+
+
+# get text file, justice level, filtered by audio (justice results, 8583 entries)
+# filtered result (8583 entries, petitioner and respondent included)
+justice_results = pd.read_csv(project_path + 'data/mixed_data_justice/case_info_justice_filtered.csv')
+docket_justice_filter = justice_results['docket'].map(str) + '_' + justice_results['justice'].map(str)
+docket_justice_filter = docket_justice_filter.to_list()  # filter (5158)
+
+
+def get_text_pe_new(filename, docket):
+    output_path = project_path + 'data/mixed_data_justice/text_data_justice_filtered/'
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        texts = json.load(f, object_pairs_hook=dict_raise_on_duplicates)
+
+    for justice, text in texts.items():
+        justice_id = justices[justice]
+        id = str(docket) + '_' + str(justice_id)
+        if id in docket_justice_filter:
+            text = re.sub(r'dict_values', '', str(text))
+            text = re.sub(r'[\[\]\'\"(),]', '', text)
+            with open(output_path + "%s_%s_petitioner" % (docket, justice_id), 'w') as f:
+                f.write('{}'.format(text))
+    return
+
+
+def get_text_re_new(filename, docket):
+    output_path = project_path + 'data/mixed_data_justice/text_data_justice_filtered/'
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        texts = json.load(f, object_pairs_hook=dict_raise_on_duplicates)
+
+    for justice, text in texts.items():
+        justice_id = justices[justice]
+        id = str(docket) + '_' + str(justice_id)
+        if id in docket_justice_filter:
+            text = re.sub(r'dict_values', '', str(text))
+            text = re.sub(r'[\[\]\'\"(),]', '', text)
+            with open(output_path + "%s_%s_respondent" % (docket, justice_id), 'w') as f:
+                f.write('{}'.format(text))
+    return
+
+
+def get_text_filtered():
+    for root, dirs, files in os.walk(project_path + 'data/cleaned_data'):
+        for file in files:
+            if file.endswith("-result-1.txt"):
+                docket_num = file[:-13]
+                input_file = os.path.join(root, file)
+                get_text_pe_new(input_file, docket_num)
+
+            if file.endswith("-result-2.txt"):
+                docket_num = file[:-13]
+                input_file = os.path.join(root, file)
+                get_text_re_new(input_file, docket_num)
+
+# get_text_filtered()
+# 8563
+
+
+
+
+
+
---
+++ code/th_17_mix_data_justice.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import numpy as np
+import os
+from sklearn.decomposition import TruncatedSVD
+import os
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+
+# 8563 entries text, fill '' >> 5158 pe, 5158 re.
+# 5209 docket-justice id, 5209 audio pitch difference
+# delete duplicated items in info file
+# final filter number: 5158
+
+# load text, info, audio data, in [docket_justice] order
+
+def load_arguments_text(info_file, text_dir, max_num_words, max_length):
+    # '/home/wenting/PycharmProjects/thesis/data/mixed_data_justice/case_info_justice_filtered.csv'
+    justice_results = pd.read_csv(info_file)
+    docket_justice_id = justice_results['docket'].map(str) + '_' + justice_results['justice'].map(str)
+    docket_justice_id = docket_justice_id.sort_values()  # sort, order
+    docket_justice_id = docket_justice_id.to_list()  # filter
+
+    arguments_petitioner = []
+    arguments_respondent = []
+
+    # all cases in order, follow csv data
+    for id in docket_justice_id:
+        pe_path = os.path.join(text_dir, '{}_petitioner'.format(id))
+        re_path = os.path.join(text_dir, '{}_respondent'.format(id))
+
+        try:
+            with open(pe_path, 'r', encoding='utf-8') as f:
+                text = f.read()
+        except IOError:
+            text = ''
+        arguments_petitioner.append(text)
+        try:
+            with open(re_path, 'r', encoding='utf-8') as f:
+                text = f.read()
+        except IOError:
+            text = ''
+        arguments_respondent.append(text)
+
+    petitioner_len = len(arguments_petitioner)
+    texts = arguments_petitioner + arguments_respondent
+
+    # vectoring
+    tokenizer = Tokenizer(num_words=max_num_words)
+    tokenizer.fit_on_texts(texts)
+    sequences = tokenizer.texts_to_sequences(texts)
+    word_index = tokenizer.word_index  # len(word_index) unique tokens
+    # padding
+    texts_pad = pad_sequences(sequences, maxlen=max_length)
+    # texts_pad[0] is petitioner arguments, texts_pad[1] is respondent arguments
+    texts_pad = np.dstack((texts_pad[:petitioner_len], texts_pad[petitioner_len:]))  # (5209, 200, 2)
+
+    return texts_pad, word_index
+
+
+# # load data
+# mix_data_path = '/home/wenting/PycharmProjects/thesis/data/mixed_data_justice/'
+# info_path = mix_data_path + 'case_info_justice_filtered.csv'
+# audio_path = mix_data_path + 'audio_filtered.csv'
+# text_path = mix_data_path + 'text_data_justice_filtered'
+# texts_pad, word_index = load_arguments_text(info_path, text_path, 20000, 200)  # (5158, 200, 2)
+# print(texts_pad.shape)
+
+
+# process data
+# fill NA with 0
+def load_structured_data(info_file):
+    # '/home/wenting/PycharmProjects/thesis/data/mixed_data_justice/case_info_justice_filtered.csv'
+    info = pd.read_csv(info_file)
+    docket_justice_id = info['docket'].map(str) + '_' + info['justice'].map(str)
+    info['docket_justice_id'] = docket_justice_id
+    info = info.sort_values(by=['docket_justice_id'])  # sort, order
+    info = info.drop(['docket_justice_id']).fillna(0)
+
+    # one-hot-encoding
+    for col in info.columns:
+        if info[col].dtype == np.float64:
+            info[col] = info[col].astype(np.int64)
+
+    cat_columns = ['decisionType', 'naturalCourt', 'term',
+                   'chief', 'petitioner', 'petitionerState',
+                   'respondent', 'respondentState', 'jurisdiction', 'adminAction',
+                   'adminActionState', 'threeJudgeFdc', 'caseOrigin', 'caseOriginState',
+                   'caseSource', 'caseSourceState', 'lcDisagreement', 'certReason',
+                   'declarationUncon', 'issue', 'issueArea', 'lawType', 'lawSupp']
+
+    info_encoded = pd.get_dummies(info, columns=cat_columns, prefix_sep='_')
+    # cat_dummies = [col for col in info_processed if '_' in col and col.split('_')[0] in cat_columns]
+    # processed_columns = list(info_processed.columns[:])  # save the orders (1492 columns)
+
+    # SVD
+    features = info_encoded.drop(['docket', 'justice', 'partyWinning', 'petitioner_vote'], axis=1)
+    target = info_encoded['petitioner_vote']
+
+    # first, try without date
+    features = features.drop(['dateDecision', 'dateArgument'], axis=1)   # (5158, 1078)
+
+    svd = TruncatedSVD(n_components=600, random_state=42)
+    features_reduced = svd.fit_transform(features)
+    # print(svd.explained_variance_ratio_.sum())  # 0.993
+    features_reduced = pd.DataFrame(features_reduced)
+
+    info_processed = pd.concat([features_reduced, target], axis=1)
+
+    return info_processed
+
+# # process test data, following same order
+# df_test_processed = pd.get_dummies(df_test, prefix_sep='_', columns=cat_columns)
+# Remove additional columns
+# for col in df_test_processed.columns:
+#     if ("__" in col) and (col.split("__")[0] in cat_columns) and col not in cat_dummies:
+#         print("Removing additional feature {}".format(col))
+#         df_test_processed.drop(col, axis=1, inplace=True)
+# for col in cat_dummies:
+#     if col not in df_test_processed.columns:
+#         print("Adding missing feature {}".format(col))
+#         df_test_processed[col] = 0
+# df_test_processed = df_test_processed[processed_columns]
+
+
+# # try xgboost
+# from xgboost import XGBClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import accuracy_score
+#
+# # split data into train and test sets
+# X_train, X_test, y_train, y_test = train_test_split(features_reduced, target, test_size=0.33, random_state=42)
+# # fit model no training data
+# model = XGBClassifier()
+# model.fit(X_train, y_train)
+# # make predictions for test data
+# y_pred = model.predict(X_test)
+# predictions = [round(value) for value in y_pred]
+# # evaluate predictions
+# accuracy = accuracy_score(y_test, predictions)
+# print("Accuracy: %.2f%%" % (accuracy * 100.0))  # 83.21%
+
+# target.value_counts()
+# 1    2812
+# 0    2346
+# thus baseline around 0.5. at most 0.54
+
+
+def load_audio_data(audio_file):
+    # '/home/wenting/PycharmProjects/thesis/data/mixed_data_justice/audio_filtered.csv'
+    audio = pd.read_csv(audio_file)
+
+    docket_justice_id = audio['docket'].map(str) + '_' + audio['justice'].map(str)
+    audio['docket_justice_id'] = docket_justice_id
+    audio = audio.sort_values(by=['docket_justice_id'])  # sort, order
+    audio = audio.drop(['docket_justice_id']).fillna(0)
+    return audio[['petitioner_pitch', 'respondent_pitch']]
+
+# add other audio files later
